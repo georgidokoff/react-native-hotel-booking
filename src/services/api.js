00604ddc47511e89d518f5f18afa39107dec6d92
GD <georgy_dkv@yahoo.com>
2026-02-13 06:28:45 +0200

    add header for content type

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 
 export const api = axios.create({
+    headers: {
+        'Content-Type': 'application/json',
+    },
     baseURL: process.env.EXPO_PUBLIC_API_URL,
     timeout: 5000, 
 });
 
 export const authApi = axios.create({
+    headers: {
+        'Content-Type': 'application/json',
+    },
     baseURL: process.env.EXPO_PUBLIC_API_URL_AUTH,
     timeout: 5000, 
 });
