6699dc351913c35903e2d69db64eb8c42992e311
GD <georgy_dkv@yahoo.com>
2026-03-01 14:00:03 +0200

    hide keyboard on search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,21 @@
-import { View, TextInput } from "react-native";
+import { View, TextInput, Keyboard } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 import { defaultTheme } from "../helpers/styleHelper";
 import { styles } from "./styles";
 
 export default function Search({ input, setInput }) {
+
+  const changeTextHandler = (value) => {
+    if (value) {
+      setTimeout(() => {
+        Keyboard.dismiss();
+      }, 2000);
+    }
+    
+    setInput(value);
+  };
+
   return (
     <View style={styles.searchContainer}>
       <Ionicons name="search" size={24} color={defaultTheme.greyColor} />
@@ -12,7 +23,7 @@ export default function Search({ input, setInput }) {
         placeholder="Search"
         style={styles.searchInput}
         value={input}
-        onChangeText={(value) => setInput(value)}
+        onChangeText={changeTextHandler}
       />
       <Ionicons
         name="options-outline"
